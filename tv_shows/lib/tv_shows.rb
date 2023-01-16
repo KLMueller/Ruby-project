46d5698cedbcd87c107a0db10b31a1df9df4ae7e
KLMueller <kelleylynnmueller@gmail.com>
2023-01-16 14:40:41 -0600

    ruby project - commit01

---
+++ tv_shows/lib/tv_shows.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "tv_shows/version"
+require_relative "cli.rb"
+
+module TvShows
+  class Error < StandardError; end
+  # Your code goes here...
+end
